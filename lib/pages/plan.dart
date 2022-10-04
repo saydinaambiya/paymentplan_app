1a7d2d6ac68a9c74d9258a6daa05215bec194659
saydinaambiya <ssyyddn@gmail.com>
2022-10-04 16:51:09 +0700

    add state

--- lib/pages/plan.dart
+++ lib/pages/plan.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:payment_app/shared/theme.dart';
 
-class PlanPage extends StatelessWidget {
+class PlanPage extends StatefulWidget {
   const PlanPage({Key? key}) : super(key: key);
 
   @override
+  State<PlanPage> createState() => _PlanPageState();
+}
+
+class _PlanPageState extends State<PlanPage> {
+  int selectedindex = -1;
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: backgroundColor,
-      body: Padding(
+    Widget headerWidget() {
+      return Padding(
         padding: const EdgeInsets.only(
           top: 50,
           left: 32,
@@ -37,6 +42,54 @@ class PlanPage extends StatelessWidget {
             )
           ],
         ),
+      );
+    }
+
+    Widget optionWidget(int index) {
+      return GestureDetector(
+        onTap: () {
+          setState(() {
+            selectedindex = index;
+          });
+        },
+        child: Container(
+          margin: const EdgeInsets.all(20),
+          padding: const EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            border: Border.all(
+                color: selectedindex == index ? blueColor : greyColor),
+            borderRadius: BorderRadius.circular(14),
+          ),
+          child: Row(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Monthly',
+                    style: titlePlanStyle,
+                  ),
+                  Text(
+                    'Good for starting up',
+                    style: descPlanStyle,
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      body: Column(
+        children: [
+          headerWidget(),
+          optionWidget(0),
+          optionWidget(1),
+          optionWidget(2),
+        ],
       ),
     );
   }
